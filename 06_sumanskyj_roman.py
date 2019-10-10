668af8184e103ad934597f46964d49a882c0cc76
Roman Shumanskiy <151195@ukr.net>
2019-10-10 19:05:34 +0300

    change file, calc 1.0

--- 06_sumanskyj_roman.py
+++ 06_sumanskyj_roman.py
@@ -1,13 +1,38 @@
-x=" Roman"
-y="Thursday"
-x="Shumanskiy"+x
-print("Good day "+x+"!"+y+" is a perfect day to learn some python.")
-a=10
-b=2
-print("a + b =",a+b)
-print("a - b =",a-b)
-print("a / b =",a/b)
-print("a * b =",a*b)
-print("a ^ b =",pow(a,b))
-print("a % b =",a%b)
-print("a // b =",a//b)
+a=input('enter operation, + - * / is available for now ').strip()
+
+if a=='+' or a=='-' or a=='*' or a=='/' :
+	b=input('enter your 1 value ').strip()
+	
+	if not b.isdigit() and not b.replace('.','',1).isdigit() and not b.lstrip('-').isdigit() and not b.lstrip('-').replace('.','',1).isdigit():
+		b=input('error! enter your 1 value ')
+		if not b.isdigit() and not b.replace('.','',1).isdigit() and not b.lstrip('-').isdigit() and not b.lstrip('-').replace('.','',1).isdigit():
+			print('You are dump') #якби можна було б виористати goto тоді б ця перевірка була б нескінченною
+			exit(0)
+	b=float(b)
+
+	c=input('enter your 2 value ').strip()
+	
+	if not c.isdigit() and not c.replace('.','',1).isdigit() and not c.lstrip('-').isdigit() and not c.lstrip('-').replace('.','',1).isdigit():
+		c=input('error! enter your 2 value ')
+		if not c.isdigit() and not c.replace('.','',1).isdigit() and not c.lstrip('-').isdigit() and not c.lstrip('-').replace('.','',1).isdigit():
+			print('You are dump')
+			exit(0)
+	c=float(c)
+	
+	if a=='+' :
+		d=b+c;
+	if a=='-' :
+		d=b-c;
+	if a=='*' :
+		d=b*c;
+	if a=='/' :
+		if c==0 :
+			print('error - DIV/0')
+			exit(0)
+		else :
+			d=b/c;
+	
+	print ('Result', b, a, c, '=', d)
+else :
+	print('operation "',a,'" is not supported for now')
+#asda
